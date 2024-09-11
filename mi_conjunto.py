31ad1163f0eb5afd5f311b4e670cd01a7ac6625e
MaldonadoValentina <a.22308051281253@cbtis128.edu.mx>
2024-09-11 07:49:38 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,7 @@
+print("Ejemplo de Conjuntos set")
+tipodeportes = {"Futbol", "Tenis", "Basquetbol"}
+print(tipodeportes)
+print("Lista Elementos del Conjuntos tipodeportes Loop for")
+print(tipodeportes)
+for maldonado in tipodeportes:
+    print(maldonado)
---
+++ mi_diccionario.py
@@ -0,0 +1,11 @@
+print("Ejemplo de Diccionario")
+susdatos = {
+    "Nombre: " : "Ariana Maldonado",
+    "Edad: " : 16,
+    "Num. de telefono: " : "656 780 2054"
+}
+print(susdatos)
+print("Ejemplo de Diccionario Loop for")
+print(susdatos)
+for maldonado, rodriguez in susdatos.items():
+    print(maldonado, rodriguez)
